e37de927104104322b5a5e7dcdbf3e955c26156d
plaban <plabanmondal02@gmail.com>
2020-12-20 11:57:02 +0530

    Bow now working but zooming animation is not happening.

--- src/main/java/plaban/mod/malicious/init/ItemInit.java
+++ src/main/java/plaban/mod/malicious/init/ItemInit.java
@@ -1,8 +1,10 @@
 package plaban.mod.malicious.init;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.BowItem;
+import net.fabricmc.fabric.api.object.builder.v1.client.model.FabricModelPredicateProviderRegistry;
+import net.minecraft.client.item.ModelPredicateProviderRegistry;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import plaban.mod.malicious.Malicious;
@@ -12,12 +14,17 @@ import plaban.mod.malicious.item.IronRodItem;
 public class ItemInit {
 
   public static final IronRodItem IRON_ROD = new IronRodItem(new FabricItemSettings().group(ItemGroup.MISC).fireproof());
-  public static final IronBowItem IRON_BOW = new IronBowItem(new FabricItemSettings().group(ItemGroup.COMBAT));
-//  public static final BowItem IRON_BOW = new BowItem(new FabricItemSettings().group(ItemGroup.COMBAT));
+  public static final IronBowItem IRON_BOW = new IronBowItem(new FabricItemSettings().group(ItemGroup.COMBAT).maxDamage(484));
 
 
   public static void init() {
     Registry.register(Registry.ITEM, new Identifier(Malicious.MOD_ID, "iron_rod"), IRON_ROD);
     Registry.register(Registry.ITEM, new Identifier(Malicious.MOD_ID, "iron_bow"), IRON_BOW);
+
+//    SOME WEIRD STUFF FOR BOW
+    Identifier pull = new Identifier("pull");
+    FabricModelPredicateProviderRegistry.register(IRON_BOW, pull, ModelPredicateProviderRegistry.get(Items.BOW, pull));
+    Identifier pulling = new Identifier("pulling");
+    FabricModelPredicateProviderRegistry.register(IRON_BOW, pulling, ModelPredicateProviderRegistry.get(Items.BOW, pulling));
   }
 }
--- src/main/java/plaban/mod/malicious/item/IronBowItem.java
+++ src/main/java/plaban/mod/malicious/item/IronBowItem.java
@@ -1,11 +1,20 @@
 package plaban.mod.malicious.item;
 
 import net.minecraft.item.BowItem;
+import net.minecraft.item.ItemStack;
 
 public class IronBowItem extends BowItem {
   public IronBowItem(Settings settings) {
     super(settings);
   }
 
+  @Override
+  public int getMaxUseTime(ItemStack stack) {
+    return 240;
+  }
 
+  @Override
+  public int getRange() {
+    return 20;
+  }
 }
