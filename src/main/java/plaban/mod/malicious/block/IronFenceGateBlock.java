be6821547dcac5f3db1d4b1364ee544cafa27dfa
plaban <plabanmondal02@gmail.com>
2020-12-20 13:04:30 +0530

    Iron Fence Gate added

---
+++ src/main/java/plaban/mod/malicious/block/IronFenceGateBlock.java
@@ -0,0 +1,9 @@
+package plaban.mod.malicious.block;
+
+import net.minecraft.block.FenceGateBlock;
+
+public class IronFenceGateBlock extends FenceGateBlock {
+  public IronFenceGateBlock(Settings settings) {
+    super(settings);
+  }
+}
--- src/main/java/plaban/mod/malicious/init/BlockInit.java
+++ src/main/java/plaban/mod/malicious/init/BlockInit.java
@@ -12,17 +12,21 @@ import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import plaban.mod.malicious.Malicious;
+import plaban.mod.malicious.block.IronFenceGateBlock;
 
 public class BlockInit {
 
-  public static final FenceBlock IRON_FENCE = new FenceBlock(FabricBlockSettings.of(Material.METAL, MaterialColor.IRON).hardness(2.0F).resistance(5.0F).requiresTool().sounds(BlockSoundGroup.METAL));
+  public static final FenceBlock IRON_FENCE = new FenceBlock(FabricBlockSettings.of(Material.METAL, MaterialColor.IRON).hardness(4.0F).resistance(6.0F).requiresTool().sounds(BlockSoundGroup.METAL));
+  public static final IronFenceGateBlock IRON_FENCE_GATE = new IronFenceGateBlock(FabricBlockSettings.of(Material.METAL, MaterialColor.IRON).hardness(4.0F).resistance(6.0F).requiresTool().sounds(BlockSoundGroup.METAL));
 
   public static void init() {
     // Block Registry
     Registry.register(Registry.BLOCK, new Identifier(Malicious.MOD_ID, "iron_fence"), IRON_FENCE);
+    Registry.register(Registry.BLOCK, new Identifier(Malicious.MOD_ID, "iron_fence_gate"), IRON_FENCE_GATE);
 
     // Block Item Registry
     Registry.register(Registry.ITEM, new Identifier(Malicious.MOD_ID, "iron_fence"), new BlockItem(IRON_FENCE, new Item.Settings().group(ItemGroup.DECORATIONS).fireproof()));
+    Registry.register(Registry.ITEM, new Identifier(Malicious.MOD_ID, "iron_fence_gate"), new BlockItem(IRON_FENCE_GATE, new Item.Settings().group(ItemGroup.REDSTONE).fireproof()));
   }
 
 }
